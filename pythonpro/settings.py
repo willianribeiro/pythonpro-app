f18a38c6cec557270f009b95b7b3d21491228be9
Willian Ribeiro <willian.ribeiro@buser.com.br>
2020-10-16 09:59:14 -0300

    Configura modo DEBUG para True apenas localmente

--- pythonpro/settings.py
+++ pythonpro/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '%5t@0n&bn14&+50bblwhmz2i1_t2d4=2g1=#1hk*g*dse3!f%p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
