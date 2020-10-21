2b871d636839b01f0208e6c4ea9a1b81ec9b325c
Willian Ribeiro <willian.ribeiro@buser.com.br>
2020-10-21 10:02:39 -0300

    Configura AWS S3 para receber os arquivos estáticos

--- pythonpro/settings.py
+++ pythonpro/settings.py
@@ -121,3 +121,37 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+
+if AWS_ACCESS_KEY_ID:
+    # Configurações do AWS S3
+    # ------------------------------------------------------------------------------
+    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400', }  # controle de tempo de cache do s3
+    AWS_PRELOAD_METADATA = True                                     # ????
+    AWS_AUTO_CREATE_BUCKET = False                                  # Não iremos criar buckets automaticamente
+    AWS_QUERYSTRING_AUTH = True                                     # Para gerar urls assinadas
+    AWS_S3_CUSTOM_DOMAIN = None                                     # vamos utilizar o próprio domínio do s3
+    AWS_DEFAULT_ACL = 'private'                                     # para que os arquivos não fiquem públicos
+
+    # Static Assets
+    # ------------------------------------------------------------------------------
+    STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'      # é uma classe da biblioteca django-s3-folder-storage que fará a gestão do static storage
+    STATIC_S3_PATH = 'static'
+    STATIC_ROOT = f'/{STATIC_S3_PATH}/'
+    STATIC_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{STATIC_S3_PATH}/'
+    ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+
+    # Upload Media Folder
+    # ------------------------------------------------------------------------------
+    DEFAULT_FILE_STORAGE = 's3_folder_storage.s3.DefaultStorage'
+    DEFAULT_S3_PATH = 'media'
+    MEDIA_ROOT = f'/{DEFAULT_S3_PATH}/'
+    MEDIA_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{DEFAULT_S3_PATH}/'
+
+    INSTALLED_APPS.append('s3_folder_storage')
+    INSTALLED_APPS.append('storages')
